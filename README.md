848d4aea9617af0b93014fa3206fa6ef784024df
Chris Allison <chris.charles.allison@gmail.com>
2016-05-02 09:51:35 +0100

    initial setup of repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# frustration
+A card game to demonstrate the [playingcards](https://github.com/ccdale/playingcards) python module
+
+requires: [wxpython](http://www.wxpython.org/)
---
+++ tests/test_card.py
@@ -0,0 +1,9 @@
+import os
+import os.path
+import sys
+
+modpath=os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0,modpath)
+
+# from cards import Card
+# from cards import Stack
